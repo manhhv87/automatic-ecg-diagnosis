b558ccf3f3b1e38c67736c55e41dd3b8a9f23897
manhhv87 <manhhv87@gmail.com>
2023-02-15 14:56:14 +0700

    add clr

---
+++ clr/config.py
@@ -0,0 +1,15 @@
+# import the necessary packages
+import os
+
+# define the minimum learning rate, maximum learning rate, batch size,
+# step size, CLR method, and number of epochs
+MIN_LR = 1e-5
+MAX_LR = 1e-3
+STEP_SIZE = 8
+CLR_METHOD = "triangular2"
+
+# define the path to the output learning rate finder plot, training
+# history plot and cyclical learning rate plot
+LRFIND_PLOT_PATH = os.path.sep.join(["output", "lrfind_plot.png"])
+TRAINING_PLOT_PATH = os.path.sep.join(["output", "training_plot.png"])
+CLR_PLOT_PATH = os.path.sep.join(["output", "clr_plot.png"])
--- train.py
+++ train.py
@@ -3,8 +3,11 @@ import tensorflow as tf
 
 from datasets import ECGSequence
 from utils import ecg_feature_extractor
+from densenet1d import _DenseBlock, _TransitionBlock
 
-# from densenet1d import _DenseBlock, _TransitionBlock
+from clr.learningratefinder import LearningRateFinder
+from clr.clr_callback import CyclicLR
+from clr import config
 
 if __name__ == "__main__":
     # Get data and train
@@ -20,7 +23,7 @@ if __name__ == "__main__":
                              'is used for validation')
     parser.add_argument('--dataset_name', type=str, default='tracings',
                         help='name of the hdf5 dataset containing tracings')
-    parser.add_argument('--epochs', type=int, default=100,
+    parser.add_argument('--epochs', type=int, default=70,
                         help='maximum number of epochs')
     parser.add_argument('--batch_size', type=int, default=32,
                         help='batch size')
@@ -28,17 +31,11 @@ if __name__ == "__main__":
                         help='learning rate')
     args = parser.parse_args()
 
-    # Optimization settings
-    callbacks = [tf.keras.callbacks.ReduceLROnPlateau(monitor='val_loss',
-                                                      factor=0.1,
-                                                      patience=7,
-                                                      min_lr=args.lr / 100),
-                 tf.keras.callbacks.EarlyStopping(patience=9,  # should be larger than the 1 in ReduceLROnPlateau
-                                                  min_delta=0.00001)]
+    # Creating training and validation sets
+    train_seq, valid_seq, train_size = ECGSequence.get_train_and_val(args.path_to_hdf5, args.dataset_name,
+                                                                     args.path_to_csv, args.batch_size, args.val_split)
 
-    train_seq, valid_seq = ECGSequence.get_train_and_val(args.path_to_hdf5, args.dataset_name, args.path_to_csv,
-                                                         args.batch_size, args.val_split)
-
+    # Creating model
     # If you are continuing an interrupted section, uncomment line bellow:
     # model = tf.keras.models.load_model('backup_model_last.hdf5',
     #                                    custom_objects={'_DenseBlock': _DenseBlock,
@@ -51,6 +48,17 @@ if __name__ == "__main__":
     x = tf.keras.layers.Dense(units=train_seq.n_classes, activation='sigmoid', kernel_initializer='he_normal')(x)
     model = tf.keras.models.Model(inputs=backbone_model.input, outputs=x)
     model.compile(loss='binary_crossentropy', optimizer=tf.keras.optimizers.Adam(args.lr))
+
+    # Optimization settings
+    cycle_rate = CyclicLR(mode=config.CLR_METHOD,
+                          base_lr=config.MIN_LR,
+                          max_lr=config.MAX_LR,
+                          step_size=config.STEP_SIZE * train_size // args.batch_size)
+
+    callbacks = [tf.keras.callbacks.EarlyStopping(patience=9,
+                                                  min_delta=0.001,
+                                                  verbose=1),
+                 cycle_rate]
 
     # Create log
     callbacks += [tf.keras.callbacks.TensorBoard(log_dir='/content/drive/MyDrive/ECG12Dataset/logs', write_graph=False),
@@ -65,7 +73,7 @@ if __name__ == "__main__":
     # Train neural network
     history = model.fit(train_seq,
                         epochs=args.epochs,
-                        initial_epoch=0,  # If you are continuing an interrupted section change here
+                        initial_epoch=0,  # If you are continuing a interrupted section change here
                         callbacks=callbacks,
                         validation_data=valid_seq,
                         verbose=1)
