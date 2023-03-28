cd0c6710fcbe9fd8923767b8d6abd3aa563c7eb6
manhhv87 <67886698+manhhv87@users.noreply.github.com>
2023-03-28 17:05:01 +0700

    Update utils.py

--- modules/utils.py
+++ modules/utils.py
@@ -4,7 +4,7 @@ from modules.model import _DenseNet
 def ecg_feature_extractor(input_layer=None, stages=None):
     backbone_model = _DenseNet(input_layer=input_layer,
                                num_outputs=None,
-                               blocks=(6, 4, 6, 0)[:stages],        # Own model
+                               blocks=(2, 2, 2, 0)[:stages],        # Own model
                                # blocks=(6, 12, 24, 16)[:stages],   # DenseNet-121
                                # blocks=(6, 12, 32, 32)[:stages],   # DenseNet-169
                                # blocks=(6, 12, 48, 32)[:stages],   # DenseNet-201
@@ -13,7 +13,7 @@ def ecg_feature_extractor(input_layer=None, stages=None):
                                # first_num_channels=64,
                                growth_rate=8,
                                # growth_rate=32,
-                               kernel_size=(6, 4, 6, 0),
+                               kernel_size=(4, 3, 4, 0),
                                # kernel_size=(3, 3, 3, 3),
                                bottleneck=True,
                                dropout_rate=None,
