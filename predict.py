9d90063c6ec25ebe71dda97586edf2f2f7b0bb16
manhhv87 <67886698+manhhv87@users.noreply.github.com>
2023-08-09 14:16:49 +0700

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,6 +1,15 @@
 import numpy as np
 import warnings
 import argparse
+import os
+
+# ''' TF_CPP_MIN_LOG_LEVEL
+# 0 = all messages are logged (default behavior)
+# 1 = INFO messages are not printed
+# 2 = INFO and WARNING messages are not printed
+# 3 = INFO, WARNING, and ERROR messages are not printed
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
 import tensorflow as tf
 
 warnings.filterwarnings("ignore")
